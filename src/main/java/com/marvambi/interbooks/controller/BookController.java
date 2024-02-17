17a21463439c61def37536e254290b7b97898bac
Marvin Ambrose <marvambi@gmail.com>
2024-02-17 11:06:38 +0100

    feat: adds findById filter

--- src/main/java/com/marvambi/interbooks/controller/BookController.java
+++ src/main/java/com/marvambi/interbooks/controller/BookController.java
@@ -2,9 +2,12 @@ package com.marvambi.interbooks.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.marvambi.interbooks.model.Book;
 import com.marvambi.interbooks.repository.BookCollectionRepository;
@@ -22,5 +25,11 @@ public class BookController {
     @GetMapping("")
     public List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable Integer id) {
+        return bookRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found!"));
     }
 }
